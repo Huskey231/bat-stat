7b1174da5507a8a5f37360f918ea573cce2b2195
bisquickman231 <whuskey6@gmail.ocm>
2019-12-11 15:52:05 -0700

    Reworked test data storage, added more Data Recorder Tests.

--- src/test/java/whuskey/DataRecorderTests.java
+++ src/test/java/whuskey/DataRecorderTests.java
@@ -3,12 +3,14 @@ package whuskey;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import java.nio.file.Files; 
+import java.nio.file.Files;
+import java.nio.CharBuffer;
 import java.nio.file.*;
 
 import whuskey.DataRecorder;
@@ -23,7 +25,7 @@ public class DataRecorderTests{
         prevFileExists = old.exists() ? true : false;
         if(prevFileExists == true){
             try{
-                Files.move(Paths.get("src\\main\\resources\\BatteryData.txt"), Paths.get("src\\test\\resources\\BatteryData.txt"));
+                Files.move(Paths.get("src\\main\\resources\\BatteryData.txt"), Paths.get("src\\main\\resources\\BatteryDataTemp.txt"));
             }
             catch(IOException io){
                 System.out.println(io + " occurred.");
@@ -46,20 +48,33 @@ public class DataRecorderTests{
 
     @Test
     public void recordNewEntryTestCheckDataRecorded(){
-
+        CharBuffer fileContents = CharBuffer.allocate(7);
+        try{
+            DataRecorder.recordNewEntry(100, 90);
+            FileReader fr = new FileReader(old);
+            fr.read(fileContents);
+        }
+        catch(Exception e){
+            System.out.println(e + " occurred.");
+        }
     }
 
     @After
     public void tearDown(){
+        
+        
         if(old != null){
             try{
-                Files.move(Paths.get("src\\test\\resources\\BatteryData.txt"), Paths.get("src\\main\\resources\\BatteryData.txt"));
+                if(old.exists()){
+                    old.delete();
+                }
+                Files.move(Paths.get("src\\main\\resources\\BatteryDataTemp.txt"), Paths.get("src\\main\\resources\\BatteryData.txt"));
             }
             catch(IOException io){
                 System.out.println(io + " occurred.");
             }
         }
-        File oldTest = new File("src\\test\\resources\\BatteryData.txt");
+        File oldTest = new File("src\\main\\resources\\BatteryDataTemp.txt");
         if(oldTest.exists()){
             oldTest.delete();
         }
